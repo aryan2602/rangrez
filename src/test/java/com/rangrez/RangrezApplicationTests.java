88baa642c6f5c5adcb813ac422d418968d6c30e2
Dhiraj Deshmukh <Dhiraj.Deshmukh@ezest.local>
2024-09-04 00:21:11 +0530

    Your msg

---
+++ src/test/java/com/rangrez/RangrezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rangrez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RangrezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
